c1203da0e88459f52677adec4957881421f4a4fb
Jamil <jamil.mustafa@training4u.it>
2019-12-20 15:59:51 +0100

    Updated About

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <Fragment>
       <h1>About This App</h1>
       <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <p>
+        React app to search github users. This is a part of "Modern React Front
+        To Back" Udemy course by @bradtraversy
+      </p>
+      <a href="https://github.com/wxyjamil/githubfinder">Link to github</a>
     </Fragment>
   );
 };
